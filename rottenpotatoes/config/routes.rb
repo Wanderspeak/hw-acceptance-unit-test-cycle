75b92738452b44ac99342af6cda404426f2f271b
Clara Bertaut <bertautc17@grinnell.edu>
2017-10-15 01:43:45 +0000

    Implemented Find Movies With Same Director link

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,6 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  
+  get '/:movie/find-similar', to: 'directors#index', as: 'director'
 end
